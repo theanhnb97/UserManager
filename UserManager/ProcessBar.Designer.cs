f79855aadec6d51fb3dea044f9fadebec0c58d5e
Thế Anh <theanhnb97@users.noreply.github.com>
2018-12-21 10:43:23 +0700

    Initial commit

---
+++ UserManager/ProcessBar.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace UserManager
+{
+    partial class ProcessBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProcessBar));
+            this.lblRam = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnTrans = new System.Windows.Forms.Button();
+            this.btnPin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.pcbRAM = new Bunifu.Framework.UI.BunifuProgressBar();
+            this.pcbCPU = new Bunifu.Framework.UI.BunifuProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblRam
+            // 
+            this.lblRam.AutoSize = true;
+            this.lblRam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRam.ForeColor = System.Drawing.Color.White;
+            this.lblRam.Location = new System.Drawing.Point(7, 77);
+            this.lblRam.Name = "lblRam";
+            this.lblRam.Size = new System.Drawing.Size(107, 25);
+            this.lblRam.TabIndex = 9;
+            this.lblRam.Text = "Loading...";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnColor);
+            this.panel1.Controls.Add(this.btnTrans);
+            this.panel1.Controls.Add(this.btnPin);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 20);
+            this.panel1.TabIndex = 13;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Transparent;
+            this.btnColor.FlatAppearance.BorderSize = 0;
+            this.btnColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColor.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnColor.ForeColor = System.Drawing.Color.White;
+            this.btnColor.Location = new System.Drawing.Point(77, -2);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(20, 22);
+            this.btnColor.TabIndex = 9;
+            this.btnColor.TabStop = false;
+            this.btnColor.Text = "D";
+            this.btnColor.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnTrans
+            // 
+            this.btnTrans.BackColor = System.Drawing.Color.Transparent;
+            this.btnTrans.FlatAppearance.BorderSize = 0;
+            this.btnTrans.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrans.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrans.ForeColor = System.Drawing.Color.White;
+            this.btnTrans.Location = new System.Drawing.Point(51, -2);
+            this.btnTrans.Name = "btnTrans";
+            this.btnTrans.Size = new System.Drawing.Size(20, 22);
+            this.btnTrans.TabIndex = 8;
+            this.btnTrans.TabStop = false;
+            this.btnTrans.Text = "T";
+            this.btnTrans.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnTrans.UseVisualStyleBackColor = false;
+            this.btnTrans.Click += new System.EventHandler(this.btnTrans_Click);
+            // 
+            // btnPin
+            // 
+            this.btnPin.BackColor = System.Drawing.Color.Transparent;
+            this.btnPin.FlatAppearance.BorderSize = 0;
+            this.btnPin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPin.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPin.ForeColor = System.Drawing.Color.White;
+            this.btnPin.Location = new System.Drawing.Point(26, -2);
+            this.btnPin.Name = "btnPin";
+            this.btnPin.Size = new System.Drawing.Size(20, 22);
+            this.btnPin.TabIndex = 7;
+            this.btnPin.TabStop = false;
+            this.btnPin.Text = "P";
+            this.btnPin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPin.UseVisualStyleBackColor = false;
+            this.btnPin.Click += new System.EventHandler(this.btnPin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(1, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 20);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.TabStop = false;
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.ForeColor = System.Drawing.Color.White;
+            this.lblCPU.Location = new System.Drawing.Point(7, 22);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(107, 25);
+            this.lblCPU.TabIndex = 10;
+            this.lblCPU.Text = "Loading...";
+            // 
+            // pcbRAM
+            // 
+            this.pcbRAM.BackColor = System.Drawing.Color.Silver;
+            this.pcbRAM.BorderRadius = 10;
+            this.pcbRAM.Location = new System.Drawing.Point(12, 104);
+            this.pcbRAM.MaximumValue = 100;
+            this.pcbRAM.Name = "pcbRAM";
+            this.pcbRAM.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pcbRAM.Size = new System.Drawing.Size(170, 16);
+            this.pcbRAM.TabIndex = 11;
+            this.pcbRAM.Value = 0;
+            // 
+            // pcbCPU
+            // 
+            this.pcbCPU.BackColor = System.Drawing.Color.Silver;
+            this.pcbCPU.BorderRadius = 10;
+            this.pcbCPU.Location = new System.Drawing.Point(12, 49);
+            this.pcbCPU.MaximumValue = 100;
+            this.pcbCPU.Name = "pcbCPU";
+            this.pcbCPU.ProgressColor = System.Drawing.Color.Lime;
+            this.pcbCPU.Size = new System.Drawing.Size(170, 16);
+            this.pcbCPU.TabIndex = 12;
+            this.pcbCPU.Value = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 15;
+            this.bunifuElipse1.TargetControl = this.btnExit;
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 0;
+            this.bunifuElipse2.TargetControl = this.panel1;
+            // 
+            // bunifuElipse3
+            // 
+            this.bunifuElipse3.ElipseRadius = 20;
+            this.bunifuElipse3.TargetControl = this;
+            // 
+            // ProcessBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(200, 140);
+            this.Controls.Add(this.lblRam);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCPU);
+            this.Controls.Add(this.pcbRAM);
+            this.Controls.Add(this.pcbCPU);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ProcessBar";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblRam;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblCPU;
+        private Bunifu.Framework.UI.BunifuProgressBar pcbRAM;
+        private Bunifu.Framework.UI.BunifuProgressBar pcbCPU;
+        private System.Windows.Forms.Timer timer1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
+        private System.Windows.Forms.Button btnPin;
+        private System.Windows.Forms.Button btnTrans;
+        private System.Windows.Forms.Button btnColor;
+    }
+}
---
+++ UserManager/ProcessBar.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserManager
+{
+    public partial class ProcessBar : Form
+    {
+        public ProcessBar()
+        {
+            InitializeComponent();
+            InitialiseCPUCounter();
+            InitializeRAMCounter();
+        }
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+        int maxRam;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            RamMax();
+            BackColor = Color.DarkCyan;
+            TransparencyKey = Color.Blue;
+            this.TopMost = true;
+            TransparentForm();
+            this.StartPosition = FormStartPosition.Manual;
+            foreach (var scrn in Screen.AllScreens)
+            {
+                if (scrn.Bounds.Contains(this.Location))
+                {
+                    this.Location = new Point(scrn.Bounds.Right - this.Width, scrn.Bounds.Top + scrn.WorkingArea.Height / 2);
+                    this.TopMost = true;
+                    return;
+                }
+            }
+            GetInfo();
+        }
+        void TransparentForm()
+        {
+            float fontSize = 12;
+            lblCPU.Font = new Font(FontFamily.GenericSansSerif, fontSize, FontStyle.Bold);
+            lblRam.Font = new Font(FontFamily.GenericSansSerif, fontSize, FontStyle.Bold);
+            lblCPU.ForeColor = Color.White;
+            lblRam.ForeColor = Color.White;
+        }
+        private void GetInfo()
+        {
+            int cpu = Convert.ToInt32(cpuCounter.NextValue());
+            int ram = Convert.ToInt32(ramCounter.NextValue());
+            int ramused = maxRam - ram;
+            int ramPercent = (int)((ramused * 1.0 / maxRam) * 100);
+            lblCPU.Text = "CPU: " + cpu + "%";
+            lblRam.Text = "RAM: " + ramused + " MB (" + ramPercent + "%) ";
+            pcbCPU.Value = (int)cpu;
+            pcbRAM.Value = ramPercent;
+            if (cpu > 90)
+            {
+                pcbCPU.ProgressColor = Color.Red;
+            }
+            else
+            {
+                if (cpu > 70) pcbCPU.ProgressColor = Color.OrangeRed;
+                else
+                {
+                    if (cpu > 40) pcbCPU.ProgressColor = Color.Salmon;
+                    else
+                        pcbCPU.ProgressColor = Color.Lime;
+                }
+            }
+            if (ramPercent > 80)
+            {
+                pcbRAM.ProgressColor = Color.Red;
+            }
+            else
+            {
+                if (ramPercent > 60) pcbRAM.ProgressColor = Color.OrangeRed;
+                else
+                {
+                    pcbRAM.ProgressColor = Color.Lime;
+                }
+            }
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetInfo();
+
+        }
+        private void InitialiseCPUCounter()
+        {
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total", true);
+        }
+        private void InitializeRAMCounter()
+        {
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes", true);
+        }
+
+        void RamMax()
+        {
+            ulong Ram = GetTotalMemoryInBytes();
+            Ram /= 1024;
+            Ram /= 1024;
+            maxRam = (int)Ram;
+        }
+
+        static ulong GetTotalMemoryInBytes()
+        {
+            return new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+        }
+
+
+
+
+
+
+
+        // design system
+        private bool mouseDown;
+        private Point lastLocation;
+        bool Pin = false;
+        private Point lastLocationPin;
+        bool changedSize = false;
+        bool hideStatus = false;
+        bool colorStatus = false; // while
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+                this.TopMost = true;
+                this.Update();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void btnPin_Click(object sender, EventArgs e)
+        {
+            ChangeDisplaySize();
+        }
+
+
+        // change display size
+        void ChangeDisplaySize()
+        {
+            if (!changedSize)
+            {
+                changedSize = true;
+                this.Size = new Size(200 * 2, 140 / 2);
+                lblRam.Location = new Point(pcbCPU.Location.X + pcbCPU.Width + 20, lblCPU.Location.Y);
+                pcbRAM.Location = new Point(pcbCPU.Location.X + pcbCPU.Width + 20, pcbCPU.Location.Y);
+            }
+            else
+            {
+                changedSize = false;
+                this.Size = new Size(200, 140);
+                lblRam.Location = new Point(pcbCPU.Location.X, pcbCPU.Location.Y + pcbCPU.Height + 15);
+                pcbRAM.Location = new Point(lblRam.Location.X, lblRam.Location.Y + pcbCPU.Height + 10);
+            }
+
+
+            if (!Pin)
+            {
+                lastLocationPin = this.Location;
+                foreach (var scrn in Screen.AllScreens)
+                {
+                    if (scrn.Bounds.Contains(this.Location))
+                    {
+                        this.Location = new Point(scrn.Bounds.Right - this.Width - this.Width / 2, scrn.Bounds.Top);
+                        this.TopMost = true;
+                        Pin = true;
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                this.Location = lastLocationPin;
+                this.TopMost = true;
+                Pin = false;
+            }
+        }
+
+        void HideParent()
+        {
+            if (!hideStatus)
+            {
+                BackColor = Color.DarkCyan;
+                TransparencyKey = Color.DarkCyan;
+                hideStatus = true;
+            }
+            else
+            {
+                BackColor = Color.DarkCyan;
+                TransparencyKey = Color.Blue;
+                hideStatus = false;
+            }
+        }
+
+        private void btnTrans_Click(object sender, EventArgs e)
+        {
+            HideParent();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (!colorStatus)
+            {
+                lblCPU.ForeColor = Color.Teal;
+                lblRam.ForeColor = Color.Teal;
+                if (!hideStatus) this.BackColor = Color.LightCoral;
+                colorStatus = true;
+            }
+            else
+            {
+                lblCPU.ForeColor = Color.White;
+                lblRam.ForeColor = Color.White;
+                if (!hideStatus) this.BackColor = Color.Teal;
+                colorStatus = false;
+            }
+        }
+    }
+}
